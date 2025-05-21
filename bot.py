3da676c3ef076fceb83ca4be1095d37651938d04
MecatronicApp <aplicacionmecatronica@gmail.com>
2025-05-21 00:24:20 -0500

    bot.py

--- bot.py
+++ bot.py
@@ -9,15 +9,15 @@ FAQ = {
     "Fechas importantes": "https://www.ecci.edu.co/cronograma-academico/",
     "Otra duda": "Por favor consulta con el coordinador o envía un correo a info@mechatronica.edu",
     "Como ver las calificaciones del corte": "Debes ingresar al sistema académico SIA y dirigirte a la sección de 'Notas'.",
-    "Cuales son las fechas limites para dar de baja una clase": "Estas fechas se publican en el cronograma académico: https://www.ecci.edu.co/cronograma-academico/",
-    "Donde realizar la evaluación de profesores": "Entra al SIA y busca la sección 'Evaluación docente'. Es obligatoria para ver notas finales.",
-    "Como descargar el recibo de la matricula": "Desde el SIA, selecciona 'Finanzas' y luego 'Recibo de matrícula'.",
-    "Precuniarios": "Consulta información sobre los cursos precuniarios aquí: https://www.ecci.edu.co/precuniarios/",
-    "Correos importantes": "Puedes ver una lista de correos de contacto aquí: https://www.ecci.edu.co/contacto/",
-    "Aulas Virtuales": "Accede a las aulas virtuales desde https://aulasvirtuales.ecci.edu.co/",
-    "Ubicacion de las sedes": "Consulta la ubicación de las sedes aquí: https://www.ecci.edu.co/nuestras-sedes/",
-    "Curso de Inglés": "Los cursos se inscriben al inicio del semestre en el SIA, en la sección de Idiomas.",
-    "Examen de Inglés": "Puedes presentar el examen de clasificación o validación. Consulta fechas en el aula virtual de idiomas."
+    "Cuales son las fechas limites para dar de baja una clase": "La fecha límite para dar de baja una clase sin beneficio económico es hasta la segunda semana del semestre.",
+    "Donde realizar la evaluación de profesores": "La ruta para la evaluación de docentes es: Evaluaciones institucionales → Evaluación estudiante ECCI.",
+    "Como descargar el recibo de la matricula": "La ruta para descargar el recibo de matrícula es: Cuenta financiera → Resumen facturas alumno → Seleccione el recibo → Generar recibo de pago.",
+    "Precuniarios": "https://www.ecci.edu.co/derechos-pecuniarios/",
+    "Correos importantes": "financiera@ecci.edu.co\nevaluame@ecci.edu.co\nasistente.mecatronicabta@ecci.edu.co",
+    "Aulas Virtuales": "https://aulas.ecci.edu.co",
+    "Ubicacion de las sedes": "https://www.ecci.edu.co/bogota/directorio-de-sedes/?sede=5/&fbclid=PAQ0xDSwKaPJVleHRuA2FlbQIxMAABp_my-CWb9QEGzYTNg3t3rwf76Rsu7vjQv5-6yBHRFpVTkSRzEfAhwhWk9Z12_aem_ShAq23B8IJ4qHJXDi4UggA",
+    "Curso de Inglés": "https://arca.ecci.edu.co/psc/arca_1/EMPLOYEE/SA/c/EC_FORM_MN.LC_CRL_FORMULARIO.GBL?&",
+    "Examen de Inglés": "https://centrodelenguas.ecci.edu.co/examenes/"
 }
 
 # Teclado principal (sin "Horario general")
